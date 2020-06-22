8dc2c3e7b2a69c906b30a2546ce80fda5d4e5cc2
MrHuberCS <huberr@casady.org>
2020-06-22 17:22:47 -0500

    Added Scripts for Player and Camera Behavior

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float carSpeed = 5.0f;
+    [SerializeField]
+    private float turnSpeed;
+    [SerializeField]
+    private float horizontalInput;
+    [SerializeField]
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * carSpeed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * forwardInput * Time.deltaTime);
+    }
+}
